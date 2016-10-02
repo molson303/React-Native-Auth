6e0a20ac74eb872c86939d98694b91862449aafb
MIchael <michael.olson756@gmail.com>
2016-10-02 12:42:02 -0600

    button showing on screen

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import firebase from 'firebase';
 import {Header} from './components/common';
+import LoginForm from './components/LoginForm';
+
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: "AIzaSyC8XSti02w4B0Jlbdn2dJsKQHpqt1vKMvI",
+    authDomain: "authentication-6efb4.firebaseapp.com",
+    databaseURL: "https://authentication-6efb4.firebaseio.com",
+    storageBucket: "authentication-6efb4.appspot.com",
+    messagingSenderId: "456466240829"
+  });
+}
+
+
   render() {
     return(
       <View>
         <Header headerText="Log In" />
-        <Text>Hello World</Text>
+        <LoginForm />
       </View>
     );
   }
---
+++ auth/src/components/LoginForm.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <CardSection>
+        <Button>
+          Log In
+        </Button>
+      </CardSection>
+    );
+  }
+}
+
+export default LoginForm;
