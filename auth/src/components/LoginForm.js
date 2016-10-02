51c01bea7e1708ce448d7da5bee1fa1581c5ad49
MIchael <michael.olson756@gmail.com>
2016-10-02 13:31:58 -0600

    text box enabled

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import { TextInput} from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 
 
 class LoginForm extends Component {
+  state = {text: ''}
+
+
   render() {
     return (
+    <Card>
+      <CardSection>
+        <TextInput
+          value = {this.state.text}
+          onChangeText={text => this.setState({text})}
+          style={{ height: 50, width: 200 }}
+          />
+      </CardSection>
+      <CardSection />
       <CardSection>
         <Button>
           Log In
         </Button>
       </CardSection>
+    </Card>
     );
   }
 }
